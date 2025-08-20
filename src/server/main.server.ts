fc60db191c53365d5a5535541076cf5608dbf179
peleiwnl <moomayle@hotmail.co.uk>
2025-08-20 03:18:25 +0100

    Add leaderstats, TBC coin collection

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,6 +1,20 @@
+const Players = game.GetService("Players");
 const ReplicatedStorage = game.GetService("ReplicatedStorage");
 const event = ReplicatedStorage.WaitForChild("RemoteEvent") as RemoteEvent;
 
-event.OnServerEvent.Connect((plr) => {
-	print(plr.Name + "Fired the Server!");
+function leaderboardSetup(player: Player) {
+	const leaderstats = new Instance("Folder");
+	leaderstats.Name = "leaderstats";
+	leaderstats.Parent = player;
+
+	const coins = new Instance("NumberValue");
+	coins.Name = "Coins";
+	coins.Value = 0;
+	coins.Parent = leaderstats;
+}
+
+event.OnServerEvent.Connect((player: Player) => {
+	print(player.Name + "Fired the Server!");
 });
+
+Players.PlayerAdded.Connect(leaderboardSetup);
