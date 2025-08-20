607186d000e5468db43549ea155b80be00521dec
peleiwnl <moomayle@hotmail.co.uk>
2025-08-20 03:05:34 +0100

    Add teleport button

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -5,10 +5,19 @@ const button: TextButton = screenGui.WaitForChild("TextButton") as TextButton;
 const label: TextLabel = screenGui.WaitForChild("TextLabel") as TextLabel;
 const ReplicatedStorage = game.GetService("ReplicatedStorage");
 const event = ReplicatedStorage.WaitForChild("RemoteEvent") as RemoteEvent;
+const character = player.Character as Model;
+const hRP = character.WaitForChild("HumanoidRootPart") as Part;
+const teleportButton: TextButton = screenGui.WaitForChild("TeleportButton") as TextButton;
+
 let counter: number = 0;
 
 button.Activated.Connect(() => {
 	counter++;
 	label.Text = tostring(counter);
 	event.FireServer();
+});
+
+teleportButton.Activated.Connect(() => {
+	print("Got here");
+	hRP.CFrame = new CFrame(0, 5, 0);
 });
